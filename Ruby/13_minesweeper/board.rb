8e44e1821fc6012398df9e83336aa2590a99b9c8
ivan Jankovic <jankoemail@gmail.com>
2019-06-08 23:49:29 -0400

    Minesweeper first commit

---
+++ Ruby/13_minesweeper/board.rb
@@ -0,0 +1,15 @@
+# $ git status
+# $ git diff
+# $ git add -A
+# $ git status
+# $ git diff --staged
+# $ git commit -m "descriptive message about changes"
+# $ git log
+
+class Board
+
+  def initialize
+  
+  end
+
+end
