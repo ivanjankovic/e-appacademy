45e569225c582588c8caa69c4509cd429543dcae
ivan Jankovic <jankoemail@gmail.com>
2019-06-11 13:40:14 -0400

    Player calss starter

--- Ruby/*02_ghost_game/v2/ghost.rb
+++ Ruby/*02_ghost_game/v2/ghost.rb
@@ -3,8 +3,11 @@ require 'byebug'
 
 class Game
 
+  attr_reader :dictionary, :fragment
+
   def initialize(*args)
     @fragment = ''
+    @char = ''
     @dictionary = create_dictionary('./dictionary.txt')
     @players = args
   end
@@ -14,14 +17,28 @@ class Game
   end
 
   def play_round
+    # @temp_dict = Set.new
+  end
 
+  def take_turn
+    until valid_play?
+      print 'input a letter: '
+      @char = gets.chomp
+    end
   end
   
   def valid_play?
-  
+    alphabet = ('a'..'z').to_a
+    
+    false if !alphabet.include?(@char)
+    false if !dictionary.any? { |word| @char == word[fragment.length] }
+    true
   end
+  
 end
 
-player1 = 'Player.new'
-player2 = 'Player.new'
+player1 = Player.new('Sima')
+player2 = Player.new('Pera')
+
 game = Game.new(player1, player2)
+debugger
---
+++ Ruby/*02_ghost_game/v2/player.rb
@@ -0,0 +1,15 @@
+class Player
+
+  def initialize(name)
+    @name = name
+  end
+
+  def guess
+
+  end
+
+  def alert_invalid_guess
+
+  end
+
+end
