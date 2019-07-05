833ba7fabbae123a3eafb6482134750c581167f7
ivan Jankovic <jankoemail@gmail.com>
2019-07-05 17:46:43 -0400

    Trainning

--- Ruby/activ/board.rb
+++ Ruby/activ/board.rb
@@ -1,6 +1,13 @@
-x = []
-  def foo(x)
-    x << "Fancy Feast"
+def array(words)
+  new_array = []
+  i = 0
+  while i < words.length
+    if (words[i].length < 6 || words[0] == 'e') && !(words[i].length < 6 && words[0] == 'e')
+      then new_array << words[i]
+    end
+    i += 1
   end
-  foo(x)
-  p x
+  return new_array
+end
+
+print array(['eggs', 'exponential', 'cat', 'ether'])
